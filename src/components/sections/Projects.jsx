6e01557241282319cb76a1889aca956e661ea7ab
starboyxx <mohammediqbal1420@gmail.com>
2025-05-27 14:38:43 +0530

    first commit

---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,164 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> Astro Fast Fashion Retailer</h3>
+              <p className="text-gray-400 mb-4">
+                Developed a responsive, full-featured Fashion Retailer website enabling users to purchase fast fashion products, browse latest collections, view best sellers and contact.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Tailwind Css"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Generative Model For Honeypot System</h3>
+              <p className="text-gray-400 mb-4">
+                ML-powered Attack Classification platform with predictive analytics
+                and interactive reports.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["HTML","CSS","JavaScript","Python", "TensorFlow","Flask"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">AOS Animation VPN Landing Page</h3>
+              <p className="text-gray-400 mb-4">
+                Created a modern and animatedfully responsive VPN landing page using Tailwind CSS and AOS(Animate on Scroll).
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["HTML", "CSS", "Tailwind Css"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Book Rental App</h3>
+              <p className="text-gray-400 mb-4">
+                Book Rental App where users can view new released and upcoming books, add books to cart, toggle dark/light mode.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Tailwind Css", ].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
